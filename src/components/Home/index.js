9d07c45442ef8b8dc9875b0350abce94a6b9ff17
swathi <uttaralaswathi1998@gmail.com>
2023-09-23 23:52:04 +0000

    assignment5

---
+++ src/components/Home/index.js
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom'
+
+import Header from '../Header'
+
+import './index.css'
+const Home = () => (
+  <>
+    <Header />
+    <div className="home-container">
+      <div className="home-content">
+        <h1 className="heading">Find The Job That Fits Your Life</h1>
+        <p className="description">
+          Millions of people are searching for jobs, salary information, company
+          reviews. Find the jibs that fits your abilities and potential.
+        </p>
+        <Link to="/jobs">
+          <button type="button" className="button">
+            Find Jobs
+          </button>
+        </Link>
+      </div>
+    </div>
+  </>
+)
+export default Home
